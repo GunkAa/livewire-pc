9c162e357938fb690ecbd6b733ca7993799a39d5
nouser <leon.uittenbogerd@gmail.com>
2024-10-22 13:27:44 +0200

    commit
    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('components.layouts.app');
+    return redirect()->route('dashboard');
 });
-
-// Route::get('/', function () {
-//     return redirect()->route('dashboard');
-// });
-
 
 Route::get('/dashboard', Dashboard::class )->name('dashboard');
 Route::get('/users', UserManager::class)->name('users');
